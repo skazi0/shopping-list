9140d02b7f4536e5326f1b06de2eb9652b7b6957
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2018-03-22 17:44:59 +0100

    added temporary hack to avoid mysql warnings

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy as SQLAlchemyBase
 from flask_restplus import Api
 
 from app.config import BaseConfig
@@ -7,6 +7,19 @@ from app.config import BaseConfig
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
 app.config.from_envvar('APP_CONFIG')
+
+# disable pooling to avoid MySQL warnings about unused connections
+# begin closed (e.g. [Warning] Aborted connection (...) (Got an error reading communication packets)
+
+# skz: monkey patch SQLAlchemy class until
+# https://github.com/mitsuhiko/flask-sqlalchemy/issues/266
+# is fixed
+from sqlalchemy.pool import NullPool
+class SQLAlchemy(SQLAlchemyBase):
+  def apply_driver_hacks(self, app, info, options):
+    super(SQLAlchemy, self).apply_driver_hacks(app, info, options)
+    options['poolclass'] = NullPool
+    options.pop('pool_size', None)
 
 db = SQLAlchemy(app)
 api = Api(app)
@@ -24,3 +37,8 @@ def date_filter(s):
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
+
+@app.teardown_appcontext
+def close_db_connections(exception=None):
+    db.session.close()
+    db.engine.dispose()
