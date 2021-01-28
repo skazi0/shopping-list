288f022141e51478c0529ce6ab902fb372ed125f
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2021-01-28 11:14:41 +0100

    added pl locale

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Row, Col } from "antd";
+import { Row, Col, ConfigProvider } from "antd";
+import pl from "antd/lib/locale/pl_PL";
 
 import List from "./components/List";
 import ToBuy from "./components/ToBuy";
@@ -10,23 +11,25 @@ import "./App.less";
 
 function App() {
   return (
-    <DataStore>
-      <Row gutter={[16, 16]}>
-        <Col xs={0} sm={24}></Col>
-      </Row>
-      <Row gutter={[16, 16]}>
-        <Col
-          lg={{ span: 8, offset: 4 }}
-          md={{ span: 10, offset: 2 }}
-          xs={{ span: 24, offset: 0 }}
-        >
-          <ToBuy />
-        </Col>
-        <Col lg={{ span: 8 }} md={{ span: 10 }} xs={{ span: 24 }}>
-          <List />
-        </Col>
-      </Row>
-    </DataStore>
+    <ConfigProvider locale={pl}>
+      <DataStore>
+        <Row gutter={[16, 16]}>
+          <Col xs={0} sm={24}></Col>
+        </Row>
+        <Row gutter={[16, 16]}>
+          <Col
+            lg={{ span: 8, offset: 4 }}
+            md={{ span: 10, offset: 2 }}
+            xs={{ span: 24, offset: 0 }}
+          >
+            <ToBuy />
+          </Col>
+          <Col lg={{ span: 8 }} md={{ span: 10 }} xs={{ span: 24 }}>
+            <List />
+          </Col>
+        </Row>
+      </DataStore>
+    </ConfigProvider>
   );
 }
 
